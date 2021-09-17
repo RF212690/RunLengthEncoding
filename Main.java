6a24d5305d62a13857edab7707fcd577f7347cba
RF212690 <90518866+RF212690@users.noreply.github.com>
2021-09-17 01:06:05 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+package RunLengthEncoding.java;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+  Scanner input= new Scanner(System.in);
+  
+  String message = input.next();
+  char temp=message.charAt(0);
+  int counter=1;  
+  String output="";
+  for(int i=1;i<message.length();i++)
+  {
+  if(message.charAt(i)==temp){
+    counter=counter+1;
+    System.out.println("a");
+    
+    
+  }
+  else{
+  output=output+counter+temp;
+  temp=message.charAt(i);
+  counter=1;
+   System.out.println("b");  
+  }
+ 
+  }
+  output=output+counter+temp;
+  int outputLength=output.length();
+  int inputLength=message.length();
+  
+  System.out.println(output);
+  System.out.println("Your input is "+inputLength+" characters long, the output is "+outputLength+" characters long, you saved a total of "+(inputLength-outputLength)+" characters");
+  }
+}
